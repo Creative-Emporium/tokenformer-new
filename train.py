2da4eeeb10b7d8f2d2f5b359fca893212748741f
Venkataram Sivaram <vesion4690@gmail.com>
2024-12-02 17:14:28 -0800

    Prettifying training progress and options interface

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ import json
 import numpy as np
 import torch
 import tqdm
+import argparse
+import sys
 
 from datasets import load_dataset
 from dataclasses import dataclass
@@ -11,13 +13,62 @@ from dataclasses import dataclass
 from transformer import TransformerConfiguration, TransformerModel
 from tokenformer import TokenFormerConfiguration, TokenFormerModel
 
-dataset = load_dataset('uonlp/CulturaX', 'en', split='train', streaming=True)
-dataset = dataset.take(5)
+# Run-time configuration
+# TODO: checkpoint and loss destinations
+# TODO: ini files
+parser = argparse.ArgumentParser()
+parser.add_argument('model', type=str, choices=[ 'transformer', 'tokenformer' ])
+parser.add_argument('language', type=str, choices=[ 'en', 'fr', 'ru', 'sp' ])
+parser.add_argument('--batch_size', type=int, default=8)
+parser.add_argument('--epochs', type=int, default=100)
+parser.add_argument('--steps_per_epoch', type=int, default=1000)
+args = parser.parse_args(sys.argv[1:])
 
-corpus = ''
-for data in dataset:
-    corpus += '\n' + data['text']
+class Dataset:
+    
+    def __init__(self, encoder, language='en', elements=100):
+        dataset = load_dataset('uonlp/CulturaX', language, split='train', streaming=True)
+        dataset = dataset.take(elements)
+        
+        self.corpus = ''
+        for data in dataset:
+            self.corpus += '\n' + data['text']
+            
+        self.corpus = encoder.encode(self.corpus)
+        self.corpus = torch.tensor(self.corpus)
 
+        self.rng = np.random.default_rng(0)
+        self.device = 'cuda:0'
+        
+    def generate_batch(self, batch_size):
+        ix = self.rng.integers(len(self.corpus) - (config.context + 1), size=batch_size)
+        X = torch.stack([ self.corpus[i : i + config.context] for i in ix ], dim=0)
+        y = torch.stack([ self.corpus[i + 1 : i + config.context + 1] for i in ix ], dim=0)
+        return X.to(self.device), y.to(self.device)
+    
+    def evaluate_perplexity(self, model):
+        tokens = self.corpus.to(self.device).unsqueeze(0)
+
+        model.eval()
+        total_loss = 0
+        total_tokens = 0
+
+        for i in tqdm.trange(0, len(tokens[0]) - config.context, config.context,
+                             desc='\tEvaluating model perplexity', ncols=100):
+            X = tokens[:, i : i + config.context]
+            y = tokens[:, i + 1 : i  + 1 + config.context]
+
+            with torch.autocast(device_type='cuda', dtype=torch.bfloat16):
+                _, loss = model(X, y)
+
+            total_loss += loss.item() * X.size(1)
+            total_tokens += X.size(1)
+            
+        average_loss = total_loss / total_tokens
+        
+        return torch.exp(torch.tensor(average_loss)).item()
+
+# TODO: util
 def parameter_count(model):
     total_params = sum(p.numel() for p in model.parameters())
     return total_params
@@ -27,135 +78,80 @@ torch.manual_seed(0)
 torch.set_float32_matmul_precision('high')
 
 # Model configuration
-
-# TODO: argparse with langugae and config options
-# TODO: perplexity
-
-config = TokenFormerConfiguration()
-model = TokenFormerModel(config)
-
-# config = TransformerConfiguration()
-# model = TransformerModel(config)
+if args.model == 'transformer':
+    config = TokenFormerConfiguration()
+    model = TokenFormerModel(config)
+elif args.model == 'tokenformer':
+    config = TransformerConfiguration()
+    model = TransformerModel(config)
+else:
+    raise NotImplementedError(f'Invalid model \'{args.model}\'')
 
 model = model.cuda()
 torch.compile(model)
 
-encoding = config.encoding
+dataset = Dataset(config.encoding)
 
-print('Corpus')
-print('  vocab size', encoding.n_vocab)
-print('  tokens', len(encoding.encode(corpus)))
-print('  model parameters:', parameter_count(model))
+print(50 * '-')
+print('Configuration:')
+print(50 * '-')
+print('\tvocab size', config.encoding.n_vocab)
+print('\ttokens', len(dataset.corpus))
+print('\tmodel parameters:', parameter_count(model))
 
-# Training set
-batch_size = 2
-train = encoding.encode(corpus)
-train = torch.tensor(train)
-
-rng = np.random.default_rng(0)
-
-# TODO: cycle batches...
-def generate_batch():
-    ix = rng.integers(len(train) - (config.context + 1), size=batch_size)
-    X = torch.stack([ train[i : i + config.context] for i in ix ], dim=0)
-    y = torch.stack([ train[i + 1 : i + config.context + 1] for i in ix ], dim=0)
-    return X, y
-
-batch_index = 0
-
-def generate_next_batch():
-    global batch_index
-    
-    stride = config.context + 1
-    start = batch_index * batch_size * stride
-    end = (batch_index + 1) * batch_size * stride
-    batch_index += 1
-    
-    if end > len(train):
-        batch_index = 0
-        return generate_next_batch()
-    
-    X = torch.stack([ train[i : i + config.context] for i in range(start, end, stride) ], dim=0)
-    y = torch.stack([ train[i + 1 : i + config.context + 1] for i in range(start, end, stride) ], dim=0)
-    
-    return X, y
-
+# TODO: args option
 opt = torch.optim.AdamW(model.parameters(), lr=5e-4)
 
-def generate_samples(model):
+# TODO: util
+def generate_samples(model, encoding):
     samples = [ 'Today the weather is quite', 'Here we stand, before our' ]
-
+    
+    print('\tText generation:')
     for s in samples:
         encoded = [ encoding.encode(s) ]
         encoded = torch.tensor(encoded).cuda()
         generated = model.generate(encoded, 20)
         generated = generated[0].tolist()
-        print('->', encoding.decode(generated))
+        string = encoding.decode(generated)
+        print('\t->', string.replace('\n', '\n\t\t'))
 
-for epoch in range(25):
-    e0 = time.time()
+t_train = time.time()
 
-    X, y = generate_batch()
-    # X, y = generate_next_batch()
+for epoch in range(args.epochs):
+    t_begin = time.time()
 
-    X = X.cuda()
-    y = y.cuda()
+    X, y = dataset.generate_batch(args.batch_size)
 
-    print(f'epoch {epoch}')
-    for i in range(100 + 1):
-        t0 = time.time()
-
+    print(50 * '-')
+    print(f'Epoch #{epoch + 1}')
+    print(50 * '-')
+    
+    bar = tqdm.tqdm(range(args.steps_per_epoch + 1),
+                    desc='\tTraining model on batch',
+                    ncols=100)
+    
+    for _ in bar:
         with torch.autocast(device_type='cuda', dtype=torch.bfloat16):
             logits, loss = model(X, y)
 
         opt.zero_grad()
-        
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-        
         opt.step()
+        
+        # Relay statistics
+        bar.set_postfix({
+            'loss': f'{loss.item() :01.4f}',
+        })
+        
+    bar.close()
 
-        t1 = time.time()
-
-        if i % 10 == 0:
-            throughput = X.numel() / (t1 - t0)
-            print(f'\tstep {i :03d} | loss = {loss.item() :01.4f} | time = {1000 * (t1 - t0) :03.2f} ms | throughput = {throughput :03.2f} tokens/sec')
-
-    e1 = time.time()
-
-    print(f'\ttime = {(e1 - e0) / 60 :.0f} min')
+    t_end = time.time()
     
-    generate_samples(model)
+    print(f'\tDelta = {(t_end - t_begin) / 60 :.0f} min, Progress = {(t_end - t_train) / 60 :.0f} min')
     
-# Perplexity evaluation
-model.eval()  # Set model to evaluation mode
-total_loss = 0
-total_tokens = 0
-
-with torch.no_grad():
-    dataset = load_dataset('uonlp/CulturaX', 'en', split='train', streaming=True)
-    dataset = dataset.take(5)
-    # TODO: shuffle?
-    
-    corpus = ''
-    for data in dataset:
-        corpus += data['text']
-    
-    # TODO: measure perplexity retention?
-    tokens = [ encoding.encode(corpus) ]
-    tokens = torch.tensor(tokens).cuda()
-
-    for i in tqdm.trange(0, len(tokens[0]) - config.context, config.context):
-        X = tokens[:, i:i + config.context]
-        y = tokens[:, i + 1:i + 1 + config.context]
-
-        with torch.autocast(device_type='cuda', dtype=torch.bfloat16):
-            logits, loss = model(X, y)
-
-        total_loss += loss.item() * X.size(1)
-        total_tokens += X.size(1)
-
-average_loss = total_loss / total_tokens
-perplexity = torch.exp(torch.tensor(average_loss)).item()
-
-print('average loss, perplexity', average_loss, perplexity)
+    # Evaluate the model
+    with torch.no_grad():
+        perplexity = dataset.evaluate_perplexity(model)
+        generate_samples(model, config.encoding)
+        print(f'\tperplexity = {perplexity}')
